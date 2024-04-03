0719655be5a2c3f3f648bc2419184f0c1da522db
Andrey <zaluckaev.a@yandex.ru>
2024-04-03 14:41:03 +0300

    task 1, ready 1.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // task 1
-const tableParsing = (/* content */) => {
-
+const tableParsing = (str) => {
+  const apps = str.trim().split('\n').splice(1, 100);
+  const playMarket = apps.map((app) => app.split(';')[2]);
+  const appStore = apps.map((app) => app.split(';')[3]);
+  const maxRaitPlay = `${Math.max(...playMarket)}`;
+  const maxRaitApp = `${Math.max(...appStore)}`;
+  const search = apps.map((app) => app.split(';')).filter((arr) => arr.includes(maxRaitApp) && arr.includes(maxRaitPlay)).flat();
+  console.log(`General top messenger: ${search[0]}, Owner: ${search[1]}`);
 };
 
 // task 2
